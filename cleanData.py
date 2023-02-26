62c333a9b4d4ebd5fc29c0363e9d7ebced7796af
Evelyn Tran <evelyntran235@gmail.com>
2023-02-26 18:00:47 -0500

    reading arrays from csvs

--- cleanData.py
+++ cleanData.py
@@ -1,21 +1,44 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib
 from matplotlib.figure import Figure
 import numpy as np
 import seaborn as sb
 import os
 
-os.chdir("Users/maya/DS3013")
+print(matplotlib.get_backend())
 
-df = pd.read_csv('Number of Victims by Age by Gender by County.csv', sep=";", encoding='cp1252')
-print(df.head())
+df = pd.read_csv('Number of Victims Female cleaned.csv')
+
 
 def visualize():
     plt.Figure(figsize=(20, 20))
-    plt.plot(range(df.shape[0]), 100)
-    plt.xticks(range(0, df.shape[0], 500), df['Incident Hour of Day'].loc[::500], rotation=45)
-    plt.xlabel('Time of Day', fontsize=18)
-    plt.ylabel('Number of Crimes', fontsize=18)
+    df1 = pd.DataFrame(df)
+
+    df2 = df1.transpose()
+    columns = ['Commercial', 'Government/Public Building and other', 'Road/Parking/Camps']
+    df2.columns = columns
+    df2.drop(index=df2.index[0], axis=0, inplace=True)
+    times = df2.index.to_numpy()
+    commercial = df2['Commercial'].to_numpy()
+    government = df2['Government/Public Building and other'].to_numpy()
+    road = df2['Road/Parking/Camps'].to_numpy()
+    print(df2)
+    print(times)
+    print(commercial)
+
+    plt.plot(times, commercial, label='Commercial')
+    plt.plot(times, government, label='Government/Public Building and other')
+    plt.plot(times, road, label='Road/Parking/Camps')
+
+    # Add labels and legend
+    plt.xlabel('Time of day')
+    plt.ylabel('Number of incidents')
+    plt.title('Incidents by time of day and location type')
+    plt.legend()
+
+    # Display plot
     plt.show()
+
 
 visualize()
